6f63b5f9b1c10f938a370d3acd4e51dd93286859
JeremyCurmi <jeremycurmi13@gmail.com>
2021-12-19 21:57:03 +0100

    setting up tests

--- test/test_client.py
+++ test/test_client.py
@@ -1,18 +1,20 @@
 import unittest
-from src import client
+from src,client import NamedEntityClient
+
 
 class TestClient(unittest.TestCase):
     # { "ents": [], "html": "some string"}
-    
+
     ## NerModelTestDouble('eng')
+
+    def setUp(self) -> None:
+        self.model = 'test'
+        self.client = NamedEntityClient(self.model)
+
     def test_get_ents_returns_dictionary_given_empty_string(self):
-        model = 'test'
-        var = client.NamedEntityClient(model)
-        ents = var.get_ents("")
+        ents = self.client.get_ents("")
         self.assertIsInstance(ents, dict)
-        
+
     def test_get_ents_returns_list_given_nonempty_string(self):
-        model = 'test'
-        var = client.NamedEntityClient(model)
-        ents = var.get_ents("This is a test")
+        ents = self.client.get_ents("This is a test")
         self.assertIsInstance(ents, list)
